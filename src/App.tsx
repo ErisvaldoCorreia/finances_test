fabd696670dadbe9c04b5b124b2114c5725bced4
Erisvaldo Correia <31773949+ErisvaldoCorreia@users.noreply.github.com>
2023-01-07 13:49:43 -0300

    base inicial implementa chamada basica para teste da api em resultados de consultas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,59 @@
+import { useState } from "react";
 import "./styles.css";
 
+interface infoProps {
+  symbol: string;
+  longName: string;
+  regularMarketPrice: string;
+  logourl: string;
+}
+
 export default function App() {
+  const [ticker, setTicker] = useState("");
+  const [info, setInfo] = useState<infoProps | null>(null);
+
+  function handleChange(event: any) {
+    setTicker(event.target.value);
+  }
+
+  const lerDados = async () => {
+    if (ticker === "") return alert("Ticker nao pode ser vazio");
+
+    const res = await fetch(
+      `https://brapi.dev/api/quote/${ticker}?range=1d&interval=1d&fundamental=true`
+    );
+    const { results } = await res.json();
+    const { symbol, longName, regularMarketPrice, logourl } = await results[0];
+    const dataFetched = {
+      symbol,
+      longName,
+      regularMarketPrice,
+      logourl
+    };
+    console.log(dataFetched);
+    setInfo(dataFetched);
+    setTicker("");
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <strong>Pesquise o valor de um Ativo</strong>
+      <form>
+        <label htmlFor="name">TICKER Bolsa: </label>
+        <input value={ticker} onChange={handleChange} id="name" />
+      </form>
+      <button onClick={lerDados}>Pesquisar</button>
+
+      {info && (
+        <div>
+          <p>Dados Recuperados:</p>
+          <strong>{`Simbolo: ${info.symbol}`}</strong>
+          <h3>{`Nome: ${info.longName}`}</h3>
+          <h5>{`Preço: ${info.regularMarketPrice}`}</h5>
+
+          <img src={`${info.logourl}`} alt={`${info.symbol}`} />
+        </div>
+      )}
     </div>
   );
 }
